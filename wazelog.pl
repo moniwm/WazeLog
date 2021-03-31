eb56a61bd2e5bfef8926a21a0f568300323be50a
LuisPeMoraRod <luismorarod98@gmail.com>
2021-03-31 08:17:26 -0600

    Add rules to separate words in sentence in a list

--- wazelog.pl
+++ wazelog.pl
@@ -21,18 +21,21 @@ concatenar([X|L1],L2,[X|L3]):- concatenar(L1,L2,L3).
 %Por ejemplo: ?- separar_oracion('Voy a ir a San José', L).     L = ["Voy", "a", "ir", "a", "San José"] 
 separar_oracion(Oracion , Lista) :- split_string(Oracion, " ", " ", Palabras), 
                                     unir_nombres_propios(Palabras, ListaInvertida), 
-                                    inversa(ListaInvertida, Lista).
+                                    inversa(ListaInvertida, Lista), !.
 
 unir_nombres_propios(P, L) :- unir_nombres_propios(P, [], L).
-unir_nombres_propios([], L, L).
-unir_nombres_propios([X|Tail], L1, L2) :- inicial_es_mayus(X), 
-                                       primer_elemento(Tail, Y), 
-                                       inicial_es_mayus(Y), 
-                                       string_concat(X, " ", X1),
-                                       string_concat(X1, Y, NombrePropio),
-                                       unir_nombres_propios(Tail, [NombrePropio|L1], L2).
-                                       
-unir_nombres_propios([X|Tail], L1, L2) :- unir_nombres_propios(Tail, [X|L1], L2).
+unir_nombres_propios([X|Tail], Aux, L) :- Aux == [], unir_nombres_propios(Tail, [X|Aux], L). %añade primera palabra siempre
+unir_nombres_propios([], L, L). %condicion de parada
+
+unir_nombres_propios([X|Tail1], [Y|Tail2], L) :- %une nombres propios en un solo elemento de la lista
+                                        inicial_es_mayus(Y), %si la última palabra añadida empieza con mayúscula
+                                        inicial_es_mayus(X), %si la próxima palabra a añadir empieza con mayúscula
+                                        string_concat(Y, " ", X1), %concatenar separadas por espacio en blanco
+                                        string_concat(X1, X, NombrePropio),
+                                        unir_nombres_propios(Tail1, [NombrePropio|Tail2], L). %añadir nuevo nombre a la lista                                       
+                                  
+unir_nombres_propios([X|Tail], L1, L2) :- unir_nombres_propios(Tail, [X|L1], L2). %añade la próxima palabra a L1
+
 
 %Verifica si la primera letra del string Palabra, es mayuscula. Para eso compara el código ASCII de dicha letra 
 %y valida si se encuentra en el rango de las letras mayúsculas: [65, 90]
