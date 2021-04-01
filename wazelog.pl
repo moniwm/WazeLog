04503452e54d73ef7c81fcaae002b5bef36c0fd8
Monica Waterhouse <mwaterhouse@estudiantec.cr>
2021-03-31 19:42:01 -0600

    Crear lógica para que aparezca la sumatoria de los tiempos entre puntos

--- wazelog.pl
+++ wazelog.pl
@@ -2,34 +2,50 @@
 %------------------------------ Definición de la base de datos ----------------------------------------------------------------
 
 
-/*conecta(origen, destino, distancia, tiempoNormal, tiempoPresa) indica que origen conecta con destino con una distancia
-de x cantidad de kilómetros y posee una lista de 2 tiempos:  el primero es el tiempo promedio en condiciones normales y el segundo es el tiempo con presa*/
+/*
+conecta(origen, destino, distancia, tiempoNormal, tiempoPresa) indica que origen conecta con destino con una distancia
+de x cantidad de kilómetros y posee una lista de 2 tiempos:  el primero es el tiempo promedio en condiciones normales y 
+el segundo es el tiempo con presa
+*/
 
-conecta(a, b, 2, [10, 20]).
-conecta(a, f, 3, [15, 30]).
-conecta(a, d, 1, [5, 10]).
-conecta(b, a, 2, [14, 28]).
-conecta(b, e, 4, [20, 40]).
-conecta(c, e, 5, [15, 30]).
-conecta(c, g, 7, [35, 70]).
-conecta(d, a, 1, [3, 6]).
-conecta(d, g, 8, [40, 80]).
-conecta(e, b, 4, [12, 24]).
-conecta(e, c, 5, [25, 50]).
-conecta(e, f, 6, [30, 60]).
-conecta(f, a, 3, [21, 42]).
-conecta(f, e, 6, [18, 36]).
-conecta(g, c, 7, [21, 42]).
-conecta(g, d, 8, [24, 48]).
+conecta(a, b, 2, 10, 20).
+conecta(a, f, 3, 15, 30).
+conecta(a, d, 1, 5, 10).
+conecta(b, a, 2, 14, 28).
+conecta(b, e, 4, 20, 40).
+conecta(c, e, 5, 15, 30).
+conecta(c, g, 7, 35, 70).
+conecta(d, a, 1, 3, 6).
+conecta(d, g, 8, 40, 80).
+conecta(e, b, 4, 12, 24).
+conecta(e, c, 5, 25, 50).
+conecta(e, f, 6, 30, 60).
+conecta(f, a, 3, 21, 42).
+conecta(f, e, 6, 18, 36).
+conecta(g, c, 7, 21, 42).
+conecta(g, d, 8, 24, 48).
 
 
-ruta(Origen, Destino, Ruta, Distancia) :- camino(Origen, Destino, [Origen], Referencia, Distancia), reverse(Referencia, Ruta).
+ruta(Origen, Destino, Ruta, Distancia, TiempoNormal, TiempoPresa) :- 
+    camino(Origen, Destino, [Origen], Referencia, Distancia, TiempoNormal, TiempoPresa), 
+    reverse(Referencia, Ruta).
 
-camino(Origen, Destino, Ruta, [Destino|Ruta], Distancia) :- conecta(Origen, Destino, Distancia, _).
+camino(Origen, Destino, Ruta, [Destino|Ruta], Distancia, TiempoNormal, TiempoPresa) :- 
+    conecta(Origen, Destino, Distancia, TiempoNormal, TiempoPresa).
 
-camino(Origen, Destino, Visitados, Ruta, Distancia) :- conecta(Origen, Intermedio, DistanciaAdyacente, _), Intermedio \== Destino, \+member(Intermedio, Visitados), camino(Intermedio, Destino,[Intermedio|Visitados], Ruta, Distancia1), Distancia is DistanciaAdyacente + Distancia1. 
+camino(Origen, Destino, Visitados, Ruta, Distancia, TiempoNormal, TiempoPresa) :- 
+    conecta(Origen, Intermedio, DistanciaAdyacente, TiempoNormalAdyacente, TiempoPresaAdyacente), 
+    Intermedio \== Destino, 
+    \+member(Intermedio, Visitados), 
+    camino(Intermedio, Destino,[Intermedio|Visitados], Ruta, Distancia1, TiempoNormal1, TiempoPresa1), 
+    Distancia is DistanciaAdyacente + Distancia1,
+    TiempoNormal is TiempoNormalAdyacente + TiempoNormal1,
+    TiempoPresa is TiempoPresaAdyacente + TiempoPresa1. 
 
-ruta_corta(Origen, Destino, Ruta, Distancia) :- setof([R,D], ruta(Origen, Destino, R, D), Set), Set = [_|_], minima(Set,[Ruta, Distancia]).
+rutaCorta(Origen, Destino, Ruta, Distancia, TiempoNormal, TiempoPresa) :- 
+    setof([R,D], ruta(Origen, Destino, R, D, TiempoNormal, TiempoPresa), Set), 
+    Set = [_|_], 
+    minima(Set,[Ruta, Distancia]).
  
 minima([F|R],M) :- min(R,F,M).
 
