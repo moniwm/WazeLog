439f99d520014e7b2688d2b9e7a28b270052e986
Monica Waterhouse <mwaterhouse@estudiantec.cr>
2021-03-31 19:16:53 -0600

    Crear regla que obtenga la ruta más corta

--- wazelog.pl
+++ wazelog.pl
@@ -3,33 +3,39 @@
 
 
 /*conecta(origen, destino, distancia, tiempoNormal, tiempoPresa) indica que origen conecta con destino con una distancia
-de x cantidad de kilómetros e indica el tiempo promedio en condiciones normales así como con presa*/
+de x cantidad de kilómetros y posee una lista de 2 tiempos:  el primero es el tiempo promedio en condiciones normales y el segundo es el tiempo con presa*/
 
-conecta(a, b, [2, 10, 20]).
-conecta(a, f, [3, 15, 30]).
-conecta(a, d, [1, 5, 10]).
-conecta(b, a, [2, 14, 28]).
-conecta(b, e, [4, 20, 40]).
-conecta(c, e, [5, 15, 30]).
-conecta(c, g, [7, 35, 70]).
-conecta(d, a, [1, 3, 6]).
-conecta(d, g, [8, 40, 80]).
-conecta(e, b, [4, 12, 24]).
-conecta(e, c, [5, 25, 50]).
-conecta(e, f, [6, 30, 60]).
-conecta(f, a, [3, 21, 42]).
-conecta(f, e, [6, 18, 36]).
-conecta(g, c, [7, 21, 42]).
-conecta(g, d, [8, 24, 48]).
-
-
-ruta(Origen, Destino, Ruta) :- camino(Origen, Destino, [Origen], Referencia), reverse(Referencia, Ruta).
-
-camino(Origen, Destino, Ruta, [Destino|Ruta]) :- conecta(Origen, Destino, _).
-
-camino(Origen, Destino, Visitados, Ruta) :- conecta(Origen, Intermedio, _), Intermedio \== Destino, \+member(Intermedio, Visitados), camino(Intermedio, Destino,[Intermedio|Visitados], Ruta). 
+conecta(a, b, 2, [10, 20]).
+conecta(a, f, 3, [15, 30]).
+conecta(a, d, 1, [5, 10]).
+conecta(b, a, 2, [14, 28]).
+conecta(b, e, 4, [20, 40]).
+conecta(c, e, 5, [15, 30]).
+conecta(c, g, 7, [35, 70]).
+conecta(d, a, 1, [3, 6]).
+conecta(d, g, 8, [40, 80]).
+conecta(e, b, 4, [12, 24]).
+conecta(e, c, 5, [25, 50]).
+conecta(e, f, 6, [30, 60]).
+conecta(f, a, 3, [21, 42]).
+conecta(f, e, 6, [18, 36]).
+conecta(g, c, 7, [21, 42]).
+conecta(g, d, 8, [24, 48]).
 
 
+ruta(Origen, Destino, Ruta, Distancia) :- camino(Origen, Destino, [Origen], Referencia, Distancia), reverse(Referencia, Ruta).
+
+camino(Origen, Destino, Ruta, [Destino|Ruta], Distancia) :- conecta(Origen, Destino, Distancia, _).
+
+camino(Origen, Destino, Visitados, Ruta, Distancia) :- conecta(Origen, Intermedio, DistanciaAdyacente, _), Intermedio \== Destino, \+member(Intermedio, Visitados), camino(Intermedio, Destino,[Intermedio|Visitados], Ruta, Distancia1), Distancia is DistanciaAdyacente + Distancia1. 
+
+ruta_corta(Origen, Destino, Ruta, Distancia) :- setof([R,D], ruta(Origen, Destino, R, D), Set), Set = [_|_], minima(Set,[Ruta, Distancia]).
+ 
+minima([F|R],M) :- min(R,F,M).
+
+min([],M,M).
+min([[P,L]|R],[_,M],Min) :- L < M, !, min(R,[P,L],Min). 
+min([_|R],M,Min) :- min(R,M,Min).
 
 %------------------------------ Sintagmas nominales y verbales ----------------------------------------------------------------
 
